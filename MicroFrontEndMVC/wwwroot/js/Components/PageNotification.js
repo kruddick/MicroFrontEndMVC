c6daa8ad8b4a1e0dc8bcf1c25259fe2222e82222
Jon Paradise <paradise.jon@live.com>
2023-01-26 09:08:04 -0600

    SignalR implementation:
    Client-side listener & exposed POST method to change the PageNotification content/color

---
+++ MicroFrontEndMVC/wwwroot/js/Components/PageNotification.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/hubs/notification").build();
+
+connection.on("PageNotification", function (request) {
+    let notificationElement = document.querySelector('#pageNotificationContainer');
+    if (notificationElement) {
+        notificationElement.style.backgroundColor = request.color;
+        notificationElement.querySelector('span').innerText = request.message;
+    }
+});
+
+connection.start();
